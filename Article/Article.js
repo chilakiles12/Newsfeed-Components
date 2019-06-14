c829604793fcc44261fe41f3b54130994251b55d
eric <Eric355402@yahoo.com>
2019-06-13 21:20:40 -0600

    almost done with article.js, domElement showing null

--- Article/Article.js
+++ Article/Article.js
@@ -1,20 +1,24 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
+  constructor(article) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
+
+    this.domElement = article.querySelector('.article');
+    // create a reference to the ".expandButton" class.
+    this.expandButton = article.querySelector('.expandButton');
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand';
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    // expandButton.addEventListener('click', expandArticle);
+    console.log(this);
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('.article-open');
   }
 }
 
@@ -26,4 +30,7 @@ class Article {
 
 */
 
-let articles;
+let articles = document
+  .querySelectorAll('.article')
+  .forEach(article => new Article(article));
+console.log(articles);
